a33bba27cd3da9778270bb2a6fa8d0772a094449
diimasputraa <203040088@mail.unpas.ac.id>
2022-11-02 18:25:23 +0700

    Penghapusan Komentar

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -14,8 +14,6 @@
         <main class="form-signin w-100 m-auto">
         <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
           <form>
-            <!-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
-        
             <div class="form-floating">
               <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
               <label for="floatingInput">Email address</label>
