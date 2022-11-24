a5bac5a8c4afca8f5c2c72a919da31babf0eed76
diimasputraa <203040088@mail.unpas.ac.id>
2022-11-24 20:01:49 +0700

    Menyelesaikan Video 22 Laravel

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -13,7 +13,7 @@
             @csrf
             <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle" class="align-text-bottom"></span> Delete</button>
         </form>
-        
+
         @if ($post->image)
         <div style="max-height: 350px; overflow:hidden;">
             <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
@@ -21,6 +21,7 @@
         @else
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
         @endif
+        
 
         <article class="my-3 fs-5">
         {!! $post->body !!}
