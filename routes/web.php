fdb9d3e5c97fa2098f0acddb85eef639c894aaf8
diimasputraa <203040088@mail.unpas.ac.id>
2022-11-25 14:09:51 +0700

    Menyelesaikan Video 23 Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 // use App\Models\Post;
 // use App\Models\User;
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+
 
 
 /*
@@ -64,4 +66,7 @@ Route::get('/dashboard', function(){
 })->middleware('auth');
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware(('admin'));
